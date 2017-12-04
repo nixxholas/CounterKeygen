b931ac525f24d5e1eddfdfe5c0b2d47febbbbab5
nixxholas <nicholas@slyck.net>
2017-12-04 18:04:27 +0800

    Source: Program Restructure

--- Program.cs
+++ Program.cs
@@ -9,15 +9,67 @@ namespace CounterKeygen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi there! Welcome to the Bitcoin private & public key generator");
-            Console.WriteLine("Please key in a jibberish string!");
+            while (true) {
+                Console.WriteLine("Hi there! Welcome to the Bitcoin key generator");
+                Console.WriteLine("==============================================");
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine();
+                Console.WriteLine("1 - Create Basic Keys");
+                Console.WriteLine("2 - Create BIP32 Compliant Keys");
+                Console.WriteLine("3 - Create BIP44 Private Keys");
+                Console.WriteLine("4 - Generate a child key from an X Key");
+                Console.WriteLine();
+                Console.WriteLine("0 - Exit");
 
-            string jibberish = Console.ReadLine();
+                while (true) {
+                    string choice = Console.ReadLine();
+                    int result = 0;
 
-            if (jibberish != null) {
+                    if (int.TryParse(choice, out result))
+                    {
+                        if (result == 0) {
+                            Console.WriteLine("Goodbye!");
+                            Environment.Exit(0);
+                        }
+
+                        switch (result) {
+                            case 1:
+                                Console.Clear();
+                                loadCreateBasicKeys();
+                                Console.Clear();
+
+                                goto BREAK;
+                            default:
+                                Console.WriteLine("Invalid input, please try again.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input, please try again.");
+                    }
+                }
+
+                BREAK:;
+            }
+        }
+
+        private static void loadCreateBasicKeys() {
+            Console.WriteLine("Generating a random key...");
+            Console.WriteLine("==============================================");
+            Console.WriteLine("Please save all the data below!");
+            Console.WriteLine("");
+
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            byte[] random = new byte[4096];
+            provider.GetBytes(random);
+            string jibberish = Encoding.Unicode.GetString(random);
+
+            if (jibberish != null)
+            {
                 // Step 1: String => Byte[] => SHA256 Byte[]
                 // Compute the jibberish
-                byte[] sha256jibberish = BitcoinKeyHelper.Sha256(Encoding.Default.GetBytes(jibberish));
+                byte[] sha256jibberish = BitcoinKeyHelper.Sha256(Encoding.Unicode.GetBytes(jibberish));
 
                 // Debugging
                 //Console.WriteLine(BitConverter.ToString(sha256jibberish).Replace("-", "").ToLower());
@@ -69,6 +121,10 @@ namespace CounterKeygen
                 byte[] pubKey = BitcoinKeyHelper.GeneratePublicKey(modifiedSha256);
 
                 Console.WriteLine("Your Public Key is: " + BitConverter.ToString(pubKey).Replace("-", "").ToLower());
+                Console.WriteLine("==============================================");
+                Console.WriteLine("Once you're done, simply enter any key to return to the menu.");
+
+                Console.ReadKey();
             }
         }
     }
